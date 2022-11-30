5fe008b92645f7d4e0c85bab5f349db85cd5152f
justin40715 <justin40715@gmail.com>
2022-11-30 19:18:13 +0800

    W13-P1: PersonContext for T82_80

---
+++ w13_hooks/src/App_80.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import T81_80 from './tutorial/8-useContext/T81_80-ContextAPI';
+
+const App_80 = () => {
+  return (
+    <div className='container'>
+      <h2>Tutorial T81_208410380</h2>
+      <T81_80 />
+    </div>
+  );
+};
+
+export default App_80;
