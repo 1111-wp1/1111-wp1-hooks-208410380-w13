66f7bc322497aea8bf6ea61d373675f734c10944
justin40715 <justin40715@gmail.com>
2022-11-30 21:04:32 +0800

    W13-P2: Apply useReducer to add an item with modal alert

--- w13_hooks/src/App_80.js
+++ w13_hooks/src/App_80.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import T81_80 from './tutorial/8-useContext/T81_80-ContextAPI';
+import T6_80 from './tutorial/6-useReducer/T6_80_useState';
 
 const App_80 = () => {
   return (
     <div className='container'>
-      <h2>Tutorial T81_208410380</h2>
-      <T81_80 />
+      <h2>Tutorial T61_208410380</h2>
+      <T6_80 />
     </div>
   );
 };
